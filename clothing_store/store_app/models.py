52fd0032e7d25df8e51d466d63a5d4f0f014b635
MadhuPojha <mpriya1201@gmail.com>
2024-05-21 17:08:04 +0200

    Product_Stuck Model created

--- clothing_store/store_app/models.py
+++ clothing_store/store_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product_Stock(models.Model):
+    pass
+    product = models.CharField(max_length=100)
+    size = models.CharField(max_length=100)
+    stock = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.product} {self.size} {self.stock}"
