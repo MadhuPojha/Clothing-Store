4072706c3c9d26b57e893ecb92eb7e5b87bb80eb
Ammara Saif ullah <ammarach045@gmail.com>
2024-05-23 12:19:21 +0200

    Cart and CartItem models are created.

--- clothing_store/store_app/models.py
+++ clothing_store/store_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Cart (models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    product = models.ManyToManyField(Product, through= 'CartItem') # need Product Class
+
+    def __str__(self):
+        return f"Cart for {self.user.username}"
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE) # need Product Class
+    quatity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"Quantity: {self.quatity} _ Product Name: {self.product.name}"
