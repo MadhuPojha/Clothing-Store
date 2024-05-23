c0c2159316711eaf34314ab5ac1be2bb1a81e7c5
Kenneth-Javier <kj.ortigoza@gmail.com>
2024-05-23 14:20:53 +0200

    add register to view

--- clothing_store/user_app/views.py
+++ clothing_store/user_app/views.py
@@ -17,3 +17,36 @@ def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
 
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'profile_pic' in request.FILES:
+                profile.profile_pic = request.FILES['profile_pic']
+
+            profile.save()
+
+            registered = True
+            if registered:
+                print("registered user: " + user.username)
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+
+    return render(request, 'user_app/register.html',
+                          {'user_form': user_form,
+                           'profile_form': profile_form,
+                           'registered': registered})
