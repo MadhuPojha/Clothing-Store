a3e55b5988c1b0436e3a4055ab3947a68286cab5
MadhuPojha <mpriya1201@gmail.com>
2024-05-23 13:18:22 +0200

    Product_stock

--- clothing_store/store_app/models.py
+++ clothing_store/store_app/models.py
@@ -3,11 +3,12 @@ from django.db import models
 # Create your models here.
 
 
-class Product_Stock(models.Model):
-    pass
-    product = models.CharField(max_length=100)
+class Product_Stock(models.Model):      #product_info 
+    
+    product = models.ForeignKey(Product, on_delete=models.CASCADE) 
     size = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
     stock = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.product} {self.size} {self.stock}"
+        return f"Product: {self.Product.product} Size: {self.size} Color:{self.color} Stock:{self.stock}"
