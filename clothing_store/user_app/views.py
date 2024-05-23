46a6f30abe4a9239241072ddbcc147241460ea5b
Kenneth-Javier <kj.ortigoza@gmail.com>
2024-05-23 14:16:12 +0200

    add index to views

--- clothing_store/user_app/views.py
+++ clothing_store/user_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from user_app.forms import UserForm, UserProfileInfoForm
+
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+#from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'user_app/index.html')
+
