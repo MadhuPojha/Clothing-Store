0b82715b8ee0efdfbf73242df9d374b7d7f5e047
Vanessa Arruda <adm.vanessasouza@gmail.com>
2024-05-23 10:03:40 +0200

    add Product Model

--- clothing_store/store_app/admin.py
+++ clothing_store/store_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from store_app.models import Product
 # Register your models here.
+admin.site.register(Product)
--- clothing_store/store_app/models.py
+++ clothing_store/store_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Product_Category) #need to add Product_Category model
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    description = models.TextField(max_length=300)
+    cover_image = models.ImageField(upload_to='product/image')
+
+    def __str__(self):
+        return f"Category {self.category} Product: {self.name} | Price: {self.price}"
