2b9e01a3c680c5c6f9c36e77d96298849a3e5373
MadhuPojha <mpriya1201@gmail.com>
2024-05-27 14:54:21 +0200

    add 3 tables in admin

--- clothing_store/store_app/admin.py
+++ clothing_store/store_app/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from store_app.models import Product, Cart, CartItem, ProductImages, Order
+from store_app.models import Product, Cart, CartItem, ProductImages, Order, Category, Order_Details, Product_Stock
 
 admin.site.register(Cart)
 admin.site.register(CartItem)
 admin.site.register(Product)
 admin.site.register(ProductImages)
 admin.site.register(Order)
+admin.site.register(Category)
+admin.site.register(Order_Details)
+admin.site.register(Product_Stock)
