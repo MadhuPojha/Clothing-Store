ac51c57c0a8d39b2d566e503a610190d5bfc41cc
NicUll <lordtardus@gmail.com>
2024-05-29 13:46:29 +0200

    Update admin.py
    Added TODO for code deletion

--- clothing_store/store_app/admin.py
+++ clothing_store/store_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from store_app.models import Product, ProductImages, Order, Category, Order_Details, Product_Stock
 # Cart, CartItem
-
+#TODO Remove unnecessary code
 # admin.site.register(Cart)
 # admin.site.register(CartItem)
 admin.site.register(Product)
