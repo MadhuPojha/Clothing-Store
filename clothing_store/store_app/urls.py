d554f9fa05dce9463f5f50e069a5d23f624fd650
ashu surana <surana.ashu@gmail.com>
2024-05-30 15:34:46 +0200

    added path to yrl.py--product,order_confirmation,return_policy,contact_us,customer_service

--- clothing_store/store_app/urls.py
+++ clothing_store/store_app/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     #path('login/', views.login, name='login'),
     path('category/', views.category, name='category'),
     path('cart/', views.cart, name='cart'),
+    path('product/', views.product, name='product'),
+    path('order_confirmation/', views.order_confirmation, name='order_confirmation'),
+    path('return_policy/', views.return_policy, name='return_policy'),
+    path('contact_us/', views.contact_us, name='contact_us'),
+    path('customer_service/', views.customer_service, name='customer_service')
+    
 ]
