2cfb0c23e7711b0e2bec883477ffa8f741e239cf
ashu surana <surana.ashu@gmail.com>
2024-05-30 15:33:05 +0200

    edited product in view and created order_confirmation inview

--- clothing_store/store_app/views.py
+++ clothing_store/store_app/views.py
@@ -8,17 +8,35 @@ def home(request):
     current_year = datetime.now().year
     return render(request, 'index.html', {'current_year': current_year})
 
+
 def product(request):
-    product = Product.objects.all()
-    return render(request, 'product.html', {'product': product})	# 'store_app/product.html'
+    product_list=Product.objects.all()
+    product_dict={'product_item':product_list}
+    return render(request,'product.html',context=product_dict)   # 'store_app/product.html'
+    	
 
 def product_stock(request):
     product_stock = Product_Stock.objects.all()
     return render(request, 'product_stock.html', {'product_stock': product_stock})	# 'store_app/product_stock.html'
 
 def category(request):
-    category = Category.objects.all()
+    categorys = Category.objects.all()
     return render(request, 'category.html', {'category': category})	# 'store_app/category.html'
+
 
 def cart(request):
     return render(request, "cart.html", {'cart': cart})
+
+def order_confirmation(request):
+    return render(request, 'order_confirmation.html', {'order_confirmation': order_confirmation})
+
+def return_policy(request):
+    return render(request, 'return_policy.html', {'return_policy': return_policy})
+
+def contact_us(request):
+    return render(request, 'contact_us.html', {'contact_us': contact_us})
+
+def customer_service(request):
+    return render(request, 'customer_service.html', {'customer_service': customer_service})
+
+
