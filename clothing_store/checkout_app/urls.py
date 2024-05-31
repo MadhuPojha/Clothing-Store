dda62be896ffa0e0b99f633fc32186948fb2fe5d
Ammara Saif ullah <ammarach045@gmail.com>
2024-05-31 15:51:00 +0200

    add order history path

--- clothing_store/checkout_app/urls.py
+++ clothing_store/checkout_app/urls.py
@@ -7,6 +7,7 @@ app_name = 'checkout_app'
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
-    path('order_summary/', views.order_summary, name='order_summary')
+    path('order_summary/', views.order_summary, name='order_summary'),
+    path('order-history/', views.order_history, name='order_history'),
     # path('order_confirmation/', views.order_confirmation, name='order_confirmation'),
 ]
